7098a1435d0cd2678271cfbca467617b1e22810d
good1luck21 <good1luck21>
2022-08-08 02:54:19 +0000

    add ajax function using rails js function for homework4

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
-    resources :favorites, only: [:create,:destroy]
+    resource :favorites, only: [:create,:destroy]
     resources :book_comments, only: [:create,:destroy, :edit, :update]
   end
   
